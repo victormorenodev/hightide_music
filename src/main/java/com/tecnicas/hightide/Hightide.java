9696f2cf2aba5aacd94cff8748d6c43cc3b8ba88
Henrique Oliveira <pedhenriqueogomes@gmail.com>
2024-09-24 02:06:30 -0300

    feat: ViewBranch

--- src/main/java/com/tecnicas/hightide/Hightide.java
+++ src/main/java/com/tecnicas/hightide/Hightide.java
@@ -9,6 +9,7 @@ import com.tecnicas.hightide.controller.PlaylistController;
 import static com.tecnicas.hightide.model.models.Musica.Genero.*;
 import com.tecnicas.hightide.model.services.MusicaService;
 import com.tecnicas.hightide.model.services.PlaylistService;
+import com.tecnicas.hightide.view.telaHightide;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.swing.JOptionPane;
@@ -20,7 +21,7 @@ import javax.swing.JOptionPane;
 public class Hightide {
 
     public static void main(String[] args) {
-        
+        /*
         MusicController musicController = new MusicController();
         PlaylistController playlistController = new PlaylistController();
         
@@ -53,5 +54,36 @@ public class Hightide {
         
         emf.close();
         MP3Player.tocarMusica(musicaUrl);
+*/
+        
+        
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(telaHightide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(telaHightide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(telaHightide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(telaHightide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new telaHightide().setVisible(true);
+            }
+        });
     }
 }
